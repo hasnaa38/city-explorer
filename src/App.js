3ffd6338f6cb478790543f1c6f20e003778f9c84
Hasnaa Al-Habahbeh <hasnaahab98@gmail.com>
2021-09-14 22:54:59 +0300

    changed to heroku after deploying apis there

--- src/App.js
+++ src/App.js
@@ -79,7 +79,8 @@ class App extends Component {
   }
 
   weatherBitAPI = () => {
-    axios.get(`http://${process.env.REACT_APP_BACKEND_URL}/weather?lat=${this.state.lat}&lon=${this.state.lon}`)
+    // https://city-explorer-api-h3.herokuapp.com/
+    axios.get(`https://city-explorer-api-h3.herokuapp.com/weather?lat=${this.state.lat}&lon=${this.state.lon}`)
       .then(res => {
         this.setState(
           {
@@ -97,8 +98,10 @@ class App extends Component {
   }
 
   moviedbAPI = () => {
-    axios.get(`http://localhost:8000/movies?city_name=${this.state.display_place}`)
+    axios.get(`https://city-explorer-api-h3.herokuapp.com/movies?city_name=${this.state.display_place}`)
       .then(res => {
+        console.log('===========')
+        console.log(res.data);
         this.setState(
           {
             moviesData: res.data,
